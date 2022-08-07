008c4da317a0e369887e5eb9c26a1afc7d3e2560
Ali354 <mahfoodali795@gmail.com>
2022-08-07 22:22:45 +0300

    file with add course is finished

--- ay-al-ma-ma/models/course.js
+++ ay-al-ma-ma/models/course.js
@@ -1,11 +1,12 @@
 class Course {
-    constructor(id, title, description, defTime, lessonsNum,owner_id) {
+    constructor(id, title, description, defTime, lessonsNum,owner_id, imgURL) {
             this.id = id;
             this.title = title;
             this.description = description;
             this.defTime = defTime;
             this.lessonsNum = lessonsNum;
             this.owner_id = owner_id;
+            this.imgURL= imgURL;
     }
 }
  
--- ay-al-ma-ma/routes/file-routes.js
+++ ay-al-ma-ma/routes/file-routes.js
@@ -3,6 +3,11 @@ const {
     addUser
     // 
    } = require('../controllers/userController.js');
+   
+const {
+    addCourse
+    // 
+   } = require('../controllers/courseController');
 const { authenticateToken ,authTokenVerifyMiddleWare} = require('../Verify/UserVerfiy.js');
 const {uploadFile} = require('../controllers/fileController');
 const router = express.Router();
@@ -10,8 +15,8 @@ const router = express.Router();
 /////
 
 
-router.post('/file',uploadFile,addUser );
-
+router.post('/addUserWithfile',uploadFile,addUser );
+router.post('/addCourseWithfile',authTokenVerifyMiddleWare,uploadFile,addCourse);
 module.exports = {
     routes: router
 }
